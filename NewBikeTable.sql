bfd5975c86bf07fb41ae48bf27407e5eae830b13
GregoryGreen <87345933+GregoryGreen@users.noreply.github.com>
2022-06-28 01:37:43 -0400

    Update NewBikeTable.sql

--- NewBikeTable.sql
+++ NewBikeTable.sql
@@ -1,4 +1,5 @@
--- SQLite
+-- Combined all 12 datasets (12 months) and removed columns: start_lat, start_lng, end_lat, end_lng, start_station_id,end_station_id, end_station_name that won't be used
+for analysis
 select ride_id, rideable_type, started_at, ended_at, start_station_name, end_station_name, member_casual
 from(
     select ride_id, rideable_type, started_at, ended_at, start_station_name, end_station_name, member_casual from "May 22 Trip"
