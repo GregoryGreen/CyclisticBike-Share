b0f71df2a5522c7e0668c4560a8ebdff322fd79b
GregoryGreen <87345933+GregoryGreen@users.noreply.github.com>
2022-07-21 21:19:39 -0400

    Update RBikeCaseStudy.R

--- RBikeCaseStudy.R
+++ RBikeCaseStudy.R
@@ -1,88 +1,375 @@
 library(tidyverse)
+library(skimr)
 library(lubridate)
 library(janitor)
-library(dplyr)
+library(scales)
 library(ggplot2)
+library(mapview)
 
-DivvyTable <- DivvyTable %>%  
-  select(-c(end_station_name)) #remove irrelevant columns not vital for analysis
+# Combine every dataset to consolidate analysis
 
-View(DivvyTable)
+trip22_June <- read.csv("/Users/gregorygreen/Downloads/202206-divvy-tripdata.csv")
+trip22_May <- read.csv("/Users/gregorygreen/Downloads/202205-divvy-tripdata.csv")
+trip22_Apr <- read.csv("/Users/gregorygreen/Downloads/202204-divvy-tripdata.csv")
+trip22_Mar <- read.csv("/Users/gregorygreen/Downloads/202203-divvy-tripdata.csv")
+trip22_Feb <- read.csv("/Users/gregorygreen/Downloads/202202-divvy-tripdata.csv")
+trip22_Jan <- read.csv("/Users/gregorygreen/Downloads/202201-divvy-tripdata.csv")
+trip21_Dec <- read.csv("/Users/gregorygreen/Downloads/202112-divvy-tripdata.csv")
+trip21_Nov <- read.csv("/Users/gregorygreen/Downloads/202111-divvy-tripdata.csv")
+trip21_Oct <- read.csv("/Users/gregorygreen/Downloads/202110-divvy-tripdata.csv")
+trip21_Sept <- read.csv("/Users/gregorygreen/Downloads/202109-divvy-tripdata.csv")
+trip21_Aug <- read.csv("/Users/gregorygreen/Downloads/202108-divvy-tripdata.csv")
+trip21_July <- read.csv("/Users/gregorygreen/Downloads/202107-divvy-tripdata.csv")
 
-colnames(DivvyTable) #List column names
-head(DivvyTable) #First 6 rows of data frame
-summary(DivvyTable) #inspect date and dimensions before moving onto cleaning
-str(DivvyTable) #view columns and datatypes
+BikeShareTable <- rbind(trip22_June, trip22_May, trip22_Apr, trip22_Mar, trip22_Feb, trip22_Jan, trip21_Dec, 
+trip21_Nov, trip21_Oct, trip21_Sept, trip21_Aug, trip21_July)
 
+write.csv(BikeShareTable, "/Users/gregorygreen/BikeShareTable.csv", row.names = FALSE)
 
-DivvyTable <- DivvyTable[complete.cases(DivvyTable), ] #remove any null values
+View(BikeShareTable)
 
-#Adding columns for date and time
-DivvyTable$date <- as.Date(DivvyTable$started_at)
-DivvyTable$month <- format(as.Date(DivvyTable$date), "%m")
-DivvyTable$day <- format(as.Date(DivvyTable$date), "%d")
-DivvyTable$year <- format(as.Date(DivvyTable$date), "%Y")
-DivvyTable$day_of_week <- format(as.Date(DivvyTable$date), "%A")
-DivvyTable$time <- format(DivvyTable$started_at, format= "%H:%M")
-DivvyTable$time <- as.POSIXct(DivvyTable$time, format= "%H:%M") # date-time conversion
+# Understand data types of the newly created data frame
+glimpse(BikeShareTable)
 
-#isolating time spent on every ride
-DivvyTable$ride_length <- (as.double(difftime(DivvyTable$ended_at, DivvyTable$started_at))) / 60
+# Statistical evaluation
+#BikeShareTable %>%
+#  skim()
 
-str(DivvyTable)
-
-summary(DivvyTable) #observing new column
-
-#Analyze data
-aggregate(DivvyTable$ride_length ~ DivvyTable$member_casual, FUN = mean)
-aggregate(DivvyTable$ride_length ~ DivvyTable$member_casual, FUN = median)
-aggregate(DivvyTable$ride_length ~ DivvyTable$member_casual, FUN = max)
-aggregate(DivvyTable$ride_length ~ DivvyTable$member_casual, FUN = min)
-
-#creating days of the week
-DivvyTable$day_of_week <- ordered(DivvyTable$day_of_week, levels=c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
-
-DivvyTable %>%
-  mutate(day_of_week = wday(started_at, label = TRUE)) %>% #create weekday
-  group_by(member_casual, day_of_week ) %>%  #groups by usertype and weekday
-  summarise(number_of_rides = n())
-
-#Remaining code: Data Visualization
-
-DivvyTable$day_of_week  <- format(as.Date(DivvyTable$date), "%A")
-DivvyTable %>%                              #total rides broken down by weekday
-  group_by(member_casual, day_of_week) %>% 
-  summarise(number_of_rides = n()) %>% 
-  arrange(member_casual, day_of_week) %>%
-  ggplot(aes(x = day_of_week, y = number_of_rides, fill = member_casual)) + geom_col(position = "dodge") + 
-  labs(x='Day of Week', y='Total Number of Rides', title='Rides per Day of Week', fill = 'Type of Membership') + 
-  scale_y_continuous(breaks = c(250000, 400000, 550000), labels = c("250K", "400K", "550K"))
-
-DivvyTable %>%   #total rides broken down by month
-  group_by(member_casual, month) %>%  
-  summarise(total_rides = n(),`average_duration_(mins)` = mean(ride_length)) %>% 
-  arrange(member_casual) %>% 
-  ggplot(aes(x=month, y=total_rides, fill = member_casual)) + geom_col(position = "dodge") + 
-  labs(x= "Month", y= "Total Number of Rides", title = "Rides per Month", fill = "Type of Membership") + 
-  scale_y_continuous(breaks = c(100000, 200000, 300000, 400000), labels = c("100K", "200K", "300K", "400K")) + theme(axis.text.x = element_text(angle = 360))
-
-DivvyTable %>% #observing breakdown of different type of bikes rented
-  ggplot(aes(x = rideable_type, fill = member_casual)) + geom_bar(position = "dodge") +
-  labs(x= 'Type of Bike', y= 'Number of Rentals', title = 'Which bike works the most', fill= 'Type of Membership') +
-  scale_y_continuous(breaks = c(500000, 1000000, 1500000), labels = c("500K", "1Mil", "1.5Mil"))
-
-DivvyTable %>% #Find average time spent riding, each membership  per day
-  mutate(day_of_week = wday(started_at, label = TRUE)) %>%
-  group_by(member_casual, day_of_week) %>%
-  summarise(number_of_rides = n()
-            ,average_duration = mean(ride_length)) %>%
-  arrange(member_casual, day_of_week) %>%
-  ggplot(aes(x = day_of_week, y = average_duration, fill = member_casual)) +
-  geom_col(position = "dodge") + labs(x='Days of the week', y='Average duration - Hrs', title='Average ride time per week', fill='Type of Membership')
+# Data Cleaning:
 
 
-View(DivvyTable)
+BikeShareTableClean = BikeShareTable %>% 
+  remove_empty(which = c("cols", "rows")) %>% 
+  clean_names()
+
+# Transforming data types
+BikeShareTable_1 = BikeShareTable %>% 
+  mutate(
+    start_lat = as.numeric(start_lat),
+    start_lng = as.numeric(start_lng),
+    end_lat = as.numeric(end_lat),
+    end_lng = as.numeric(end_lng)
+  )
+
+glimpse(BikeShareTable_1)
 
 
+colSums(is.na(BikeShareTable_1))
 
-write.csv(DivvyTable, "/Users/gregorygreen/DivvyTable.csv")
+# Evaluate statistical summary
+BikeShareTable_1 %>%
+  summary()
+
+# Format date-time columns
+BikeShareTable_2 = BikeShareTable_1 %>%
+  mutate(
+    started_at = ymd_hms(as_datetime(started_at)),
+    ended_at = ymd_hms(as_datetime(ended_at))
+  )
+glimpse(BikeShareTable_2)
+
+# Creating additional columns from started_at dat-time column
+
+BikeShareTable_3 = BikeShareTable_2 %>%
+  mutate(
+    hour_start = hour(started_at),
+    weekday = wday(started_at, label = T, abbr = F),
+    month = month(started_at, label = T, abbr = F),
+    day = day(started_at),
+    week = strftime(started_at, format = "%V"),
+    trip_time = difftime(ended_at, started_at, units = "mins")
+  )
+
+glimpse(BikeShareTable_3)
+
+# Convert bike and ride types
+
+BikeShareTable_4 = BikeShareTable_3 %>%
+  mutate(
+    rideable_type = recode(as_factor(rideable_type),
+                           "classic_bike" = "classic",
+                           "electric_bike" = "electric",
+                           "docked_bike" = "docked"),
+    member_casual = as_factor(member_casual)
+  )
+glimpse(BikeShareTable_4)
+
+BikeShareTable_4 = BikeShareTable_4 %>%
+  rename(
+    bikes = rideable_type,
+    users = member_casual
+  )
+
+#BikeShareTable_4 %>% 
+  #get_dupes(ride_id) %>% 
+  #tally()
+View(BikeShareTable_4)
+
+# Create new data frame
+BikeShareTable_5 = BikeShareTable_4 %>%
+  filter(
+    between(trip_time, 1, 1440)
+  )
+
+str(BikeShareTable_5)
+
+head(BikeShareTable_5, 10)
+
+trips_time_df = BikeShareTable_5 %>%
+  drop_na(
+    end_lat, end_lng
+  ) %>% 
+  select(
+    ride_id, users, bikes, hour_start, weekday, month, day, week, trip_time
+  )
+colSums(is.na(trips_time_df))
+View(trips_time_df)
+
+# Create dataframe with location variables
+
+trips_location_df = BikeShareTable_5 %>% 
+  select(
+    ride_id, start_station_name, end_station_name, start_lat, start_lng,
+    end_lat, end_lng, users, trip_time
+  ) %>% 
+  drop_na(
+    start_station_name, end_station_name
+  )
+
+colSums(is.na(trips_location_df))
+
+
+# Data Visualization
+
+newtheme <- theme_light() + 
+  theme(plot.title = element_text(color = "#002949", face = 'bold', size =12),
+        plot.subtitle = element_text(color = "#890000", size = 10),
+        plot.caption = element_text(color = '#890000', face = 'italic', size =8),
+        panel.border = element_rect(color = "#002949", size = 1),
+        legend.position = "right",
+        legend.text = element_text(colour="blue", size=10, face="bold"),
+        legend.title = element_text(colour="blue", size=10, face="bold"),
+        #legend.position='none',
+        axis.title.x = element_text(colour = "#890000"),
+        axis.title.y = element_text(colour = "#002949"),
+        axis.text.x = element_text(angle = 45, hjust = 1, color = '#890000'),
+        axis.text.y = element_text(angle = 45, hjust = 1, color = '#002949'),
+        axis.line = element_line(color = "#002949", size =1),
+  )
+
+theme_set(newtheme)
+
+# Create new summarisation variables nr_rides, average_trip, total trip grouped by users type and start hour
+
+ride_hours = 
+  trips_time_df %>% 
+  group_by(
+    users, hour_start
+  ) %>% 
+  summarise(
+    nr_rides = n(),
+    average_trip = mean(trip_time),
+    total_trip = sum(trip_time)
+  )
+
+# Visualize average number of trips per hour
+ride_hours %>% 
+  ggplot(aes(hour_start, average_trip, fill = users))+ 
+  geom_col(position = "dodge")+ 
+  scale_y_continuous()+
+  labs(
+    title = "Average Number of Trips per Hour",
+    subtitle = "Number of trips for every hour segmented by users",
+    caption = "Figure 2",
+    x = "hour of the day",
+    y = "average trips duration",
+  )+
+  theme()
+
+# Analysis: days of the week
+
+ride_week = trips_time_df %>% 
+  group_by(
+    users, weekday
+  ) %>% 
+  summarise(
+    nr_rides_week = n(),
+    avg_rides_week = mean(trip_time),
+    total_duration_week = sum(trip_time)
+  )
+
+# Visualize average trips time by day of the week
+ride_week %>% 
+  ggplot(aes(weekday, avg_rides_week, fill = users))+
+  geom_col(position = "dodge")+
+  scale_y_continuous(labels = comma)+
+  labs(
+    title = "Average Trips Time by Week Days and Segmented by Users",
+    subtitle = "Average Number of trips for every week of the year",
+    caption = "Fig 5",
+    x = "day of the week",
+    y = " avg number of trips"
+  )+
+  theme()
+
+# Visualize total trips time by day of the week
+ride_week %>% 
+  ggplot(aes(weekday, total_duration_week, fill = users))+
+  geom_col(position = "dodge")+
+  scale_y_continuous(labels = comma)+
+  labs(
+    title = "Total Time Trips by Week Days and Segmented by Users",
+    subtitle = "Total Trips Time for every week of the year",
+    caption = "Fig 6",
+    x = "day of the week",
+    y = " total time trips"
+  )+
+  theme()
+
+# Analysis: trip time by month
+ride_month = trips_time_df %>% 
+  group_by(
+    users, month
+  ) %>% 
+  summarise(
+    nr_rides_month = n(),
+    avg_rides_month = mean(trip_time),
+    total_time_month = sum(trip_time)
+  )
+
+# Visualize Total trips time by month
+ride_month %>% 
+  ggplot(aes(month, nr_rides_month, fill = users))+
+  geom_col(position = "dodge")+
+  scale_y_continuous(labels = comma)+
+  labs(
+    title = "Number of Trips by Month and Segmented by Users",
+    subtitle = "Number Trips Time for every Month",
+    caption = "Fig 7",
+    x = "month",
+    y = " number of trips"
+  )+
+  theme()
+
+
+# Analysis: type of bikes
+
+ride_bikes = trips_time_df %>% 
+  group_by(
+    users, bikes
+  ) %>% 
+  summarise(
+    nr_bike_ride = n(),
+    avg_bike_ride = mean(trip_time),
+    total_time_month = sum(trip_time)
+  )
+
+# Visualize number of rides by bike type
+ride_bikes %>% 
+  ggplot(aes(bikes,nr_bike_ride, fill = users))+
+  geom_col(position = "dodge")+
+  scale_y_continuous(labels = comma)+
+  labs(
+    title = "Number of Trips per Bike Type and Segregated by Users",
+    subtitle = "Number of trips per bike type",
+    caption = "Fig 15",
+    x = "bike type",
+    y = "number of trips"
+  )+
+  theme()
+
+# Visualize average trip time by bike type
+ride_bikes %>% 
+  ggplot(aes(bikes, avg_bike_ride, fill = users))+
+  geom_col(position = "dodge")+
+  scale_y_continuous(labels = comma)+
+  labs(
+    title = "Average trip time per Bike Type and Segregated by Users",
+    subtitle = "Average trip time per bike type",
+    caption = "Fig 16 - Paul Juverdeanu",
+    x = "bike type",
+    y = "average trip time"
+  )+
+  theme()
+
+# Analysis: Popular start stations
+pop_start_station = trips_location_df %>% 
+  group_by(
+    users, start_station_name, start_lat, start_lng
+  ) %>% 
+  summarise(
+    nr_rides_start = n()
+  ) %>% 
+  arrange(-nr_rides_start)
+
+head(pop_start_station)
+
+# Visualize the most popular 10 start stations
+pop_start_station[1:10, ] %>% 
+  ggplot(aes(start_station_name, nr_rides_start, fill = users))+
+  geom_col(position = "dodge")+
+  coord_flip()+
+  labs(
+    title = "Most Popular Start Stations",
+    subtitle = "Top 10 most popular start stations",
+    caption = "Fig 18 - Paul Juverdeanu",
+    x = "station name",
+    y = "number of trips"
+  )+
+  theme()
+
+
+# Visualize the most popular 10 end stations
+pop_end_station = trips_location_df %>% 
+  group_by(
+    users, end_station_name, end_lat, end_lng
+  ) %>% 
+  summarise(
+    nr_rides_end = n()
+  ) %>% 
+  arrange(-nr_rides_end)
+
+# Visualize the most popular 10 end stations
+pop_end_station[1:10,] %>% 
+  ggplot(aes(end_station_name, nr_rides_end, fill = users))+
+  geom_col(position = "dodge")+
+  coord_flip()+
+  labs(
+    title = "Most Popular End Stations Segmented by Users",
+    subtitle = "Top 10 most popular end stations",
+    caption = "Fig 19",
+    x = "station name",
+    y = "number of trips"
+  )+
+  theme()
+
+
+# Mapview of the most popular 30 start stations
+pop_start_station[1:30, ] %>%
+  mapview(
+    xcol = "start_lng", 
+    ycol = "start_lat",
+    cex = "nr_rides_start",
+    alpha = 0.9, 
+    crs = 4269,
+    color = "#8b0000",
+    grid = F, 
+    legend = T,
+    layer.name = "30 Most Popular Start Stations"
+  )
+
+# Mapview of the most popular 30 end stations
+pop_end_station[1:30,] %>% 
+  mapview(
+    xcol = "end_lng",
+    ycol = "end_lat",
+    cex = "nr_rides_end", # size of circle based on value size
+    alpha = 0.9,
+    crs = 4269,
+    color = "#8b0000",
+    grid = F,
+    legend = T,
+    layer.name = "30 Most Popular End Stations"
+  )
+
+
